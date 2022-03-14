874e81ebb2503bc3adfe54104ff8bc8603e5a048
raffael marinho <silvamarinho10@gmail.com>
2022-03-13 21:33:52 -0300

    feat: requirement 2 done

---
+++ desafio2.sql
@@ -0,0 +1,7 @@
+SELECT 
+	COUNT( DISTINCT m.musica_id) cancoes,
+    COUNT( DISTINCT a.artista_id) artistas,
+    COUNT( DISTINCT al.album_id) albuns
+FROM SpotifyClone.musicas m
+JOIN SpotifyClone.artistas a ON m.artista_id = a.artista_id
+JOIN SpotifyClone.albums al ON al.artista_id = a.artista_id;
